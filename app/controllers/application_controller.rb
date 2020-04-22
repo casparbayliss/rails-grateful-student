c28658ea9b80956d26533732a9de9c9843d5474c
Caspar Bayliss <casparbayliss@gmail.com>
2020-04-22 10:02:01 +0100

    pushed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_teacher!
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |teacher_params|
+      teacher_params.permit({ roles: ['Charterhouse', 'Eton', 'Cranleigh'] }, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
+  end
 end
